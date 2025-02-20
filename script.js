786c3dc4be7b4a5811159fac0814e7703753793b
megittige <meghana.mrb@gmail.com>
2025-02-20 16:11:16 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,1 @@
-document.getElementById('myButton').addEventListener('click', function() {
-    document.getElementById('content').innerText = 'Button clicked! Here is some dynamic content.';
-});
+// Add any JavaScript functionality here
